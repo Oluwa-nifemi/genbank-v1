658bef16f8a1cbaccbe0663a60e6c0fb9d1ed9a8
Nifemi <deyeminifemi@gmail.com>
2021-09-21 21:43:33 +0100

    Hid background image on mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -95,7 +95,7 @@ const IndexPage = () => (
   <>
     <Header/>
     <main className='relative'>
-      <img src={backgroundPattern} className="absolute w-full top-0 left-0 pointer-events-none" alt="Background pattern" />
+      <img src={backgroundPattern} className="absolute w-full top-0 left-0 pointer-events-none md-max:hidden" alt="Background pattern" />
       <section className='pt-28 container flex flex-col items-center first-section md-max:pt-12 relative'>
         <h1 className='font-plex-serif text-hero font-medium mb-8 w-62% text-white text-center md-max:text-hero-mobile md-max:w-100% md-max:mb-10'>
           Open a US Bank account from <Typewriter/>
