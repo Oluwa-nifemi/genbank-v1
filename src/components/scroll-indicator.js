dd363dc112dc29d0525cfea7d2ef44c802e09276
Nifemi <deyeminifemi@gmail.com>
2021-09-21 20:38:05 +0100

    Made scroll indicator scroll

--- src/components/scroll-indicator.js
+++ src/components/scroll-indicator.js
@@ -2,13 +2,21 @@ import React from "react"
 import ArrowDown from "../assets/images/arrow-down.inline.svg"
 
 const ScrollIndicator = () => {
+  const onScroll = () => {
+    window.scrollTo({
+      left: 0,
+      top: window.innerHeight,
+      behavior: "smooth"
+    })
+  }
+
   return (
-    <div className='bg-white backdrop-filter backdrop-blur-sm w-24 h-24 bg-opacity-12 rounded-half flex flex-col items-center justify-center'>
+    <button onClick={onScroll} className='bg-white backdrop-filter backdrop-blur-sm w-24 h-24 bg-opacity-12 rounded-half flex flex-col items-center justify-center border-none'>
       <span className='text-base text-white font-plex-hebrew mb-1 w-11'>
         Scroll down
       </span>
       <ArrowDown/>
-    </div>
+    </button>
   )
 }
 
