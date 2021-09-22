6875beffd67104696cd4dfdc849444e9edab2010
Nifemi <deyeminifemi@gmail.com>
2021-09-22 17:57:51 +0100

    Input component

---
+++ src/components/input.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+const Input = ({ label, value, onChange, id }) => {
+  return (
+    <div>
+      <label className='text-lg text-white font-plex-hebrew mb-3 flex' htmlFor={id}>
+        {label}
+      </label>
+      <input
+        type="text"
+        className="h-18 flex items-center text-lg font-plex-hebrew w-100% pl-4 bg-blue-800 text-white border border-transparent duration-300 focus:border-blue-200 focus:outline-none"
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  )
+}
+
+export default Input
