32aaa4f2593c6b94fdbe5aba13f1fbcc08ad9703
Nifemi <deyeminifemi@gmail.com>
2021-09-25 08:13:48 +0100

    Setup base button animation

--- src/components/button.js
+++ src/components/button.js
@@ -1,20 +1,21 @@
 import React from "react"
 import ArrowRight from "../assets/images/arrow-right.inline.svg"
 import { classNames } from "../util/functions"
+import './button.scss'
 
 const Button = ({ children, className, elevated, hasWhiteBackground, onClick }) => {
   const colorsClasses = {
-    button: hasWhiteBackground ? "text-blue-400 bg-white" : "text-blue-100 bg-blue-400",
+    button: hasWhiteBackground ? "text-blue-400 bg-white white-button" : "text-blue-100 bg-blue-400",
     arrow: hasWhiteBackground ? "text-blue-400" : "text-white",
-    elevation: hasWhiteBackground ? "border-white" : "border-blue-400"
+    elevation: hasWhiteBackground ? "border-white group-hover:border-blue-400" : "border-blue-400 group-hover:border-white"
   }
 
   return (
-    <div className="relative">
+    <div className="relative group">
       <button
         className={
           classNames(
-            "px-4 cursor-pointer py-3 text-base font-plex-hebrew flex items-center justify-between md-max:text-sm max-w-100%",
+            "px-4 cursor-pointer py-3 text-base font-plex-hebrew flex items-center md-max:text-sm max-w-100% action-button",
             elevated && "focus:transform focus:translate-x-1 focus:translate-y-1 z-10 relative duration-150",
             colorsClasses.button,
             className
@@ -22,13 +23,17 @@ const Button = ({ children, className, elevated, hasWhiteBackground, onClick }) 
         }
         onClick={onClick}
       >
-        {children}
-        <ArrowRight className={classNames("md-max:w-4 md-max:h-4", colorsClasses.arrow)} />
+        <span className='text-wrapper'>
+          <span className='text'>
+            {children}
+            <ArrowRight className={classNames("md-max:w-4 md-max:h-4 icon", colorsClasses.arrow)} />
+          </span>
+        </span>
       </button>
       {
         elevated && (
           <span
-            className={classNames("absolute w-100% h-100% left-0 top-0 transform translate-x-1 translate-y-1 border", colorsClasses.elevation)} />
+            className={classNames("absolute w-100% h-100% duration-300 left-0 top-0 transform translate-x-1 translate-y-1 border", colorsClasses.elevation)} />
         )
       }
     </div>
