05bf3e82147b6e2e9125c4116e5443a03a044ae3
Nifemi <deyeminifemi@gmail.com>
2021-09-25 09:47:02 +0100

    Added ID to input

--- src/components/input.js
+++ src/components/input.js
@@ -11,6 +11,7 @@ const Input = ({ label, value, onChange, id }) => {
         className="h-18 flex items-center text-lg font-plex-hebrew w-100% pl-4 bg-blue-800 text-white border border-transparent duration-300 outline-none focus:border-blue-200 focus:outline-none md-max:h-14"
         value={value}
         onChange={onChange}
+        id={id}
       />
     </div>
   )
