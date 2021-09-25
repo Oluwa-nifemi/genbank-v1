87d49eef0f165058eb0884d144d461d100e53e42
Nifemi <deyeminifemi@gmail.com>
2021-09-25 12:22:38 +0100

    Add env variable and used it

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const postData = async (formData) => {
   await axios.get(
-    "https://script.google.com/macros/s/AKfycbwdrS2O4FOvSIlQVwlOscqwaDzdeM2Rk8TrzERgpK73iwWZk7dFAmmTCquzM_kNpC0R/exec",
+    process.env.GATSBY_SHEET_LINK,
     {
       params: formData
     }
