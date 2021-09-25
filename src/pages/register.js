a17583a7e2d53ad492fed4a22acff7e48316cc18
Nifemi <deyeminifemi@gmail.com>
2021-09-25 09:30:55 +0100

    Connect form values to state

--- src/pages/register.js
+++ src/pages/register.js
@@ -7,9 +7,9 @@ import Checkbox from "../components/checkbox"
 import Button from "../components/button"
 import PhoneNoInput from "../components/PhoneInput"
 import countriesWithISO from "../util/phoneInputData"
-import './register.css'
-import Lottie from 'react-lottie'
-import successAnimation from '../assets/animations/success-form.json'
+import "./register.css"
+import Lottie from "react-lottie"
+import successAnimation from "../assets/animations/success-form.json"
 
 const defaultOptions = {
   loop: false,
@@ -18,21 +18,44 @@ const defaultOptions = {
   rendererSettings: {
     preserveAspectRatio: "xMidYMid slice"
   }
-};
+}
 
 const Register = () => {
   const [agree, setAgree] = useState(false)
-  const [phoneNumber, setPhoneNumber] = useState({
-    country: countriesWithISO[2] //TODO: Default to +1
+  const [formValues, setFormValues] = useState({
+    "First Name": "",
+    "Last Name": "",
+    "Phone Number": {
+      country: countriesWithISO[2],
+      number: ""
+    },
+    "Email": ""
   })
+  const [showSuccess, setShowSuccess] = useState(false)
 
-  const [showSuccess, setShowSuccess] = useState(false);
+  const setPhoneNumber = (newPhoneNumber) => {
+    setFormValues(prevState => ({
+      ...prevState,
+      "Phone Number": {
+        ...prevState["Phone Number"],
+        ...newPhoneNumber
+      }
+    }))
+  }
+
+  const onInputChange = ({ target }) => {
+    setFormValues(prevState => ({
+      ...prevState,
+      [target.id]: target.value
+    }))
+  }
 
   const toggleAgree = () => {
     setAgree(prevState => !prevState)
   }
 
   const onSubmit = () => {
+    console.log(formValues)
     setShowSuccess(true)
     window.scrollTo({
       top: 0,
@@ -42,16 +65,16 @@ const Register = () => {
 
   const renderSuccess = () => {
     return (
-      <div className='flex items-center flex-col'>
+      <div className="flex items-center flex-col">
         <Lottie
           options={defaultOptions}
           height={378}
           width={378}
         />
-        <h3 className='formHeader font-plex-hebrew text-white mb-4'>
+        <h3 className="formHeader font-plex-hebrew text-white mb-4">
           You are pre-qualified!
         </h3>
-        <p className='text-xl md:text-base font-plex-hebrew text-white'>
+        <p className="text-xl md:text-base font-plex-hebrew text-white">
           Thank you for signing up.
         </p>
       </div>
@@ -64,24 +87,34 @@ const Register = () => {
         <h1 className="formHeader text-white font-plex-serif mb-8">
           Pre-qualification form
         </h1>
-        <div className="grid grid-cols-2 gap-x-8 gap-y-10 mb-10 w-55% lg-max:w-80% md-max:w-100% md-max:px-6 md-max:grid-cols-1 md-max:gap-x-6">
+        <div
+          className="grid grid-cols-2 gap-x-8 gap-y-10 mb-10 w-55% lg-max:w-80% md-max:w-100% md-max:px-6 md-max:grid-cols-1 md-max:gap-x-6">
           <Input
             label="First Name"
             name="first-name"
+            id="First Name"
+            onChange={onInputChange}
+            value={formValues["First Name"]}
           />
           <Input
             label="Last name"
             name="last-name"
+            id="Last Name"
+            onChange={onInputChange}
+            value={formValues["Last Name"]}
           />
           <PhoneNoInput
             label="Phone number"
-            value={phoneNumber}
+            value={formValues["Phone Number"]}
             onChange={setPhoneNumber}
-            id='phoneNumber'
+            id="phoneNumber"
           />
           <Input
             label="Email address"
             name="email"
+            id="Email"
+            onChange={onInputChange}
+            value={formValues["Email"]}
           />
         </div>
         <div className="w-55% lg-max:w-80% md-max:w-100% md-max:px-6">
