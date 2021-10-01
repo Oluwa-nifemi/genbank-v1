9e6d0fbbb5f6cb8fb1fbb5dce135b4bc660bc07e
Nifemi <deyeminifemi@gmail.com>
2021-10-01 05:13:57 +0100

    Setup email function

---
+++ functions/send-contact-email/send-contact-email.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+const { SENDGRID_API_KEY } = process.env
+const sendgrid = require('@sendgrid/mail')
+const { template } = require('./template')
+
+sendgrid.setApiKey(SENDGRID_API_KEY)
+
+exports.handler = async (event) => {
+  console.log(SENDGRID_API_KEY, 's')
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed', headers: { 'Allow': 'POST' } }
+  }
+
+  const data = JSON.parse(event.body)
+  if (!data.firstName || !data.targetEmail) {
+    return { statusCode: 422, body: 'Name, email, and message are required.' }
+  }
+
+  const msg = {
+    to: data.targetEmail,
+    from: 'deyeminifemi@gmail.com',
+    subject: `Welcome to Genbank ${data.firstName}!`,
+    html: template.replaceAll('{first_name}', data.firstName),
+  }
+
+  try{
+    await sendgrid.send(msg)
+    return {
+      statusCode: 200,
+      body: "Email sent successfully"
+    }
+  }catch (e) {
+    return {
+      statusCode: 400,
+      body: "Something went wrong while sending the emails. Kindly retry or contact us at electronic@genbank.us"
+    }
+  }
+}
---
+++ functions/send-contact-email/template.js
@@ -0,0 +1,518 @@
+exports.template = `
+<html><head></head>
+<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+<head>
+  <title>
+
+  </title>
+  <!--[if !mso]><!-->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <!--<![endif]-->
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style type="text/css">
+    #outlook a { padding:0; }
+    body { margin:0;padding:0;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%; }
+    table, td { border-collapse:collapse;mso-table-lspace:0pt;mso-table-rspace:0pt; }
+    img { border:0;height:auto;line-height:100%; outline:none;text-decoration:none;-ms-interpolation-mode:bicubic; }
+    p { display:block;margin:13px 0; }
+  </style>
+  <!--[if mso]>
+  <noscript>
+    <xml>
+      <o:OfficeDocumentSettings>
+        <o:AllowPNG/>
+        <o:PixelsPerInch>96</o:PixelsPerInch>
+      </o:OfficeDocumentSettings>
+    </xml>
+  </noscript>
+  <![endif]-->
+  <!--[if lte mso 11]>
+  <style type="text/css">
+    .mj-outlook-group-fix { width:100% !important; }
+  </style>
+  <![endif]-->
+
+  <!--[if !mso]><!-->
+
+  <style type="text/css">
+    @media screen {
+      @font-face{
+        font-family:'Plex-Hebrew-Semibold';
+        font-style:normal;
+        font-weight: 400;
+        src:local('Plex-Hebrew-Semibold'), local('Plex-Hebrew-Semibold'), url('https://res.cloudinary.com/dpgdjfckl/raw/upload/v1632944739/genbank/ibm-plex-hebrew/IBMPlexSansHebrew-SemiBold_jdcpat.woff') format('woff');
+      }
+
+      @font-face{
+        font-family:'Plex-Hebrew';
+        font-style:normal;
+        font-weight: 600;
+        src:local('Plex-Hebrew'), local('Plex-Hebrew'), url('https://res.cloudinary.com/dpgdjfckl/raw/upload/v1632944738/genbank/ibm-plex-hebrew/IBMPlexSansHebrew-Regular_ayybed.woff') format('woff');
+      }
+
+      @font-face{
+        font-family:'Plex-Serif';
+        font-style:normal;
+        font-weight: 600;
+        src:local('Plex-Serif'), local('Plex-Serif'), url('https://res.cloudinary.com/dpgdjfckl/raw/upload/v1632946014/genbank/ibm-plex-hebrew/IBMPlexSerif-Regular_gymhal.ttf') format('woff');
+      }
+    }
+  </style>
+  <!--<![endif]-->
+
+
+
+  <style type="text/css">
+    @media only screen and (min-width:480px) {
+      .mj-column-per-100 { width:100% !important; max-width: 100%; }
+      .mj-column-per-50 { width:50% !important; max-width: 50%; }
+    }
+  </style>
+  <style media="screen and (min-width:480px)">
+    .moz-text-html .mj-column-per-100 { width:100% !important; max-width: 100%; }
+    .moz-text-html .mj-column-per-50 { width:50% !important; max-width: 50%; }
+  </style>
+
+
+  <style type="text/css">
+
+
+
+    @media only screen and (max-width:480px) {
+      table.mj-full-width-mobile { width: 100% !important; }
+      td.mj-full-width-mobile { width: auto !important; }
+    }
+
+  </style>
+
+
+</head>
+<body style="word-spacing:normal;background-color:#F2F5F8;">
+
+<div style="display:none;font-size:1px;color:#ffffff;line-height:1px;max-height:0px;max-width:0px;opacity:0;overflow:hidden;">
+  Congratulations {first_name}
+</div>
+
+
+<div
+  style="background-color:#F2F5F8;"
+>
+
+
+  <!--[if mso | IE]><table align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
+
+
+  <div  style="margin:0px auto;max-width:600px;">
+
+    <table
+      align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;"
+    >
+      <tbody>
+      <tr>
+        <td
+          style="direction:ltr;font-size:0px;padding:4% 0 0 0;text-align:center;"
+        >
+          <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:top;width:600px;" ><![endif]-->
+
+          <div
+            class="mj-column-per-100 mj-outlook-group-fix" style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;"
+          >
+
+            <table
+              border="0" cellpadding="0" cellspacing="0" role="presentation" width="100%"
+            >
+              <tbody>
+              <tr>
+                <td  style="vertical-align:top;padding-left:56px;">
+
+                  <table
+                    border="0" cellpadding="0" cellspacing="0" role="presentation" style="" width="100%"
+                  >
+                    <tbody>
+
+                    <tr>
+                      <td
+                        align="left" style="font-size:0px;padding:10px 25px;padding-bottom:24px;word-break:break-word;"
+                      >
+
+                        <table
+                          border="0" cellpadding="0" cellspacing="0" role="presentation" style="border-collapse:collapse;border-spacing:0px;"
+                        >
+                          <tbody>
+                          <tr>
+                            <td  style="width:100px;">
+
+                              <img
+                                height="auto" src="https://res.cloudinary.com/dpgdjfckl/image/upload/v1632944736/genbank/logo_cd32l6.png" style="border:0;display:block;outline:none;text-decoration:none;height:auto;width:100%;font-size:13px;" width="100"
+                              />
+
+                            </td>
+                          </tr>
+                          </tbody>
+                        </table>
+
+                      </td>
+                    </tr>
+
+                    </tbody>
+                  </table>
+
+                </td>
+              </tr>
+              </tbody>
+            </table>
+
+          </div>
+
+          <!--[if mso | IE]></td></tr></table><![endif]-->
+        </td>
+      </tr>
+      </tbody>
+    </table>
+
+  </div>
+
+
+  <!--[if mso | IE]></td></tr></table><table align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
+
+
+  <div  style="margin:0px auto;max-width:600px;">
+
+    <table
+      align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;"
+    >
+      <tbody>
+      <tr>
+        <td
+          style="direction:ltr;font-size:0px;padding:0 4% 0 4%;text-align:center;"
+        >
+          <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:top;width:592px;" ><![endif]-->
+
+          <div
+            class="mj-column-per-100 mj-outlook-group-fix" style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;"
+          >
+
+            <table
+              border="0" cellpadding="0" cellspacing="0" role="presentation" width="100%"
+            >
+              <tbody>
+              <tr>
+                <td  style="background-color:white;vertical-align:top;padding:4%;">
+
+                  <table
+                    border="0" cellpadding="0" cellspacing="0" role="presentation" style="" width="100%"
+                  >
+                    <tbody>
+
+                    <tr>
+                      <td
+                        align="left" style="font-size:0px;padding:10px 25px;padding-bottom:24px;word-break:break-word;"
+                      >
+
+                        <div
+                          style="font-family:Plex-Hebrew-Semibold;font-size:18px;line-height:1.5;text-align:left;color:#011B49;"
+                        >Congratulations {first_name}🎉</div>
+
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td
+                        align="left" style="font-size:0px;padding:10px 25px;padding-bottom:8px;word-break:break-word;"
+                      >
+
+                        <div
+                          style="font-family:Plex-Hebrew;font-size:16px;line-height:1;text-align:left;color:#000E25;"
+                        >You have been pre-qualified for Genbank.</div>
+
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td
+                        align="left" style="font-size:0px;padding:10px 25px;padding-bottom:8px;word-break:break-word;"
+                      >
+
+                        <div
+                          style="font-family:Plex-Hebrew;font-size:16px;line-height:1;text-align:left;color:#000E25;"
+                        >Genbank is a financial technology company that offers banking services to individuals and businesses
+                          in Africa. All banking services will be provided by our partner bank, an FDIC member.</div>
+
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td
+                        align="left" style="font-size:0px;padding:10px 25px;padding-bottom:8px;word-break:break-word;"
+                      >
+
+                        <div
+                          style="font-family:Plex-Hebrew;font-size:16px;line-height:1;text-align:left;color:#000E25;"
+                        >Our vision is to be a leading provider of borderless financial solutions.</div>
+
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td
+                        align="left" style="font-size:0px;padding:10px 25px;padding-bottom:8px;word-break:break-word;"
+                      >
+
+                        <div
+                          style="font-family:Plex-Hebrew;font-size:16px;line-height:1;text-align:left;color:#000E25;"
+                        >Like you, we are looking forward to the launch of the first American neobank that includes Africans.
+                          You will enjoy rewards for signing up early, and be the first to know when Genbank is available.</div>
+
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td
+                        align="left" style="font-size:0px;padding:10px 25px;padding-bottom:8px;word-break:break-word;"
+                      >
+
+                        <div
+                          style="font-family:Plex-Hebrew;font-size:16px;line-height:1;text-align:left;color:#000E25;"
+                        >We are excited and looking forward to serving you.</div>
+
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td
+                        align="left" style="font-size:0px;padding:10px 25px;padding-bottom:16px;word-break:break-word;"
+                      >
+
+                        <div
+                          style="font-family:Plex-Hebrew;font-size:16px;line-height:1;text-align:left;color:#000E25;"
+                        >Best regards,</div>
+
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td
+                        align="left" style="font-size:0px;padding:10px 25px;padding-bottom:4px;word-break:break-word;"
+                      >
+
+                        <div
+                          style="font-family:Plex-Serif;font-size:16px;line-height:1;text-align:left;color:#000E25;"
+                        >Ayo Allu</div>
+
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td
+                        align="left" style="font-size:0px;padding:10px 25px;padding-bottom:16px;word-break:break-word;"
+                      >
+
+                        <div
+                          style="font-family:Plex-Hebrew;font-size:16px;line-height:1;text-align:left;color:#000E25;"
+                        >CEO, Genbank.</div>
+
+                      </td>
+                    </tr>
+
+                    </tbody>
+                  </table>
+
+                </td>
+              </tr>
+              </tbody>
+            </table>
+
+          </div>
+
+          <!--[if mso | IE]></td></tr></table><![endif]-->
+        </td>
+      </tr>
+      </tbody>
+    </table>
+
+  </div>
+
+
+  <!--[if mso | IE]></td></tr></table><table align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
+
+
+  <div  style="margin:0px auto;max-width:600px;">
+
+    <table
+      align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;"
+    >
+      <tbody>
+      <tr>
+        <td
+          style="direction:ltr;font-size:0px;padding:0 4% 0 4%;text-align:center;"
+        >
+          <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:top;width:592px;" ><![endif]-->
+
+          <div
+            class="mj-column-per-100 mj-outlook-group-fix" style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;"
+          >
+
+            <table
+              border="0" cellpadding="0" cellspacing="0" role="presentation" width="100%"
+            >
+              <tbody>
+              <tr>
+                <td  style="background-color:#F7F9FC;vertical-align:top;padding:4% 4% 0 4%;">
+
+                  <table
+                    border="0" cellpadding="0" cellspacing="0" role="presentation" style="" width="100%"
+                  >
+                    <tbody>
+
+                    <tr>
+                      <td
+                        align="left" style="font-size:0px;padding:10px 25px;word-break:break-word;"
+                      >
+
+                        <div
+                          style="font-family:Plex-Hebrew;font-size:14px;line-height:1;text-align:left;color:#000E25;"
+                        >Follow us on social media for regular updates while you wait.</div>
+
+                      </td>
+                    </tr>
+
+                    </tbody>
+                  </table>
+
+                </td>
+              </tr>
+              </tbody>
+            </table>
+
+          </div>
+
+          <!--[if mso | IE]></td></tr></table><![endif]-->
+        </td>
+      </tr>
+      </tbody>
+    </table>
+
+  </div>
+
+
+  <!--[if mso | IE]></td></tr></table><table align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
+
+
+  <div  style="margin:0px auto;max-width:600px;">
+
+    <table
+      align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;"
+    >
+      <tbody>
+      <tr>
+        <td
+          style="direction:ltr;font-size:0px;padding:0 4% 4% 4%;text-align:center;"
+        >
+          <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:top;width:296px;" ><![endif]-->
+
+          <div
+            class="mj-column-per-50 mj-outlook-group-fix" style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;"
+          >
+
+            <table
+              border="0" cellpadding="0" cellspacing="0" role="presentation" width="100%"
+            >
+              <tbody>
+              <tr>
+                <td  style="background-color:#F7F9FC;border-radius:0px 0px 0px 4px;vertical-align:top;padding-left:4%;">
+
+                  <table
+                    border="0" cellpadding="0" cellspacing="0" role="presentation" style="" width="100%"
+                  >
+                    <tbody>
+
+                    <tr>
+                      <td
+                        align="left" style="font-size:0px;padding:10px 25px;padding-bottom:24px;word-break:break-word;"
+                      >
+
+                        <table
+                          border="0" cellpadding="0" cellspacing="0" role="presentation" style="border-collapse:collapse;border-spacing:0px;"
+                        >
+                          <tbody>
+                          <tr>
+                            <td  style="width:100px;">
+
+                              <img
+                                height="auto" src="https://res.cloudinary.com/dpgdjfckl/image/upload/v1632944736/genbank/logo_cd32l6.png" style="border:0;display:block;outline:none;text-decoration:none;height:auto;width:100%;font-size:13px;" width="100"
+                              />
+
+                            </td>
+                          </tr>
+                          </tbody>
+                        </table>
+
+                      </td>
+                    </tr>
+
+                    </tbody>
+                  </table>
+
+                </td>
+              </tr>
+              </tbody>
+            </table>
+
+          </div>
+
+          <!--[if mso | IE]></td><td class="" style="vertical-align:center;width:296px;" ><![endif]-->
+
+          <div
+            class="mj-column-per-50 mj-outlook-group-fix" style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:center;width:100%;"
+          >
+
+            <table
+              border="0" cellpadding="0" cellspacing="0" role="presentation" width="100%"
+            >
+              <tbody>
+              <tr>
+                <td  style="background-color:#F7F9FC;border-radius:0px 0px 4px 0px;vertical-align:center;padding-top:10px;padding-bottom:21px;padding-left:4%;">
+
+                  <table
+                    border="0" cellpadding="0" cellspacing="0" role="presentation" style="" width="100%"
+                  >
+                    <tbody>
+                    <a href="https://twitter.com/GenbankUS" style="text-decoration: none">
+                      <img src="https://res.cloudinary.com/dpgdjfckl/image/upload/v1633044277/genbank/twitter_eddrld.svg" alt="Genbank Twitter" style="padding-left: 50px; padding-right: 27px">
+                    </a>
+                    <a href="https://www.instagram.com/genbankus/" style="text-decoration: none">
+                      <img src="https://res.cloudinary.com/dpgdjfckl/image/upload/v1633044277/genbank/instagram_kmkcqc.svg" alt="Genbank Instagram" style="padding-right: 27px">
+                    </a>
+                    <a href="https://www.linkedin.com/company/genbankus/" style="text-decoration: none">
+                      <img src="https://res.cloudinary.com/dpgdjfckl/image/upload/v1633044066/genbank/linkedin_ofpoxs.svg" alt="Genbank LinkedIn">
+                    </a>
+                    </tbody>
+                  </table>
+
+                </td>
+              </tr>
+              </tbody>
+            </table>
+
+          </div>
+
+          <!--[if mso | IE]></td></tr></table><![endif]-->
+        </td>
+      </tr>
+      </tbody>
+    </table>
+
+  </div>
+
+
+  <!--[if mso | IE]></td></tr></table><![endif]-->
+
+
+</div>
+
+</body>
+</html>
+</html>
+`
